082f48b7bc3ffe320d439c379391c35eff826b29
Manish Bishnoi <manishbishnoi897@gmail.com>
2021-05-13 00:03:44 +0530

    Changed base URL

--- whatsapp-frontend/src/axios.js
+++ whatsapp-frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://whatsapp-web.herokuapp.com/",
+  baseURL: "https://whatsapp-web.herokuapp.com",
   withCredentials: true,
 });
 export default instance;
