57aecea651e9b23d10da247fba27fed710b1825e
Manish Bishnoi <manishbishnoi897@gmail.com>
2021-05-13 02:27:19 +0530

    Added credentials

--- whatsapp-frontend/src/axios.js
+++ whatsapp-frontend/src/axios.js
@@ -3,5 +3,6 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://whatsapp-web.herokuapp.com",
   withCredentials: true,
+  credentials: "include",
 });
 export default instance;
